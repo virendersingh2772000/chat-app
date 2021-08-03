ec571a4b591209d09ec1b6c41dfd65767a456424
virender <sidhu2772000@gmail.com>
2021-08-03 15:06:10 +0530

    created public and private routes

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router';
+
+const PrivateRoute = ({ children, ...routeProps }) => {
+  const profile = false;
+
+  if (!profile) {
+    return <Redirect to="/signin" />;
+  }
+
+  return (
+    <div>
+      <Route {...routeProps}>{children}</Route>
+    </div>
+  );
+};
+
+export default PrivateRoute;
---
+++ src/components/PublicRoute.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router';
+
+const PublicRoute = ({ children, ...routeProps }) => {
+  const profile = false;
+
+  if (profile) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <div>
+      <Route {...routeProps}>{children}</Route>
+    </div>
+  );
+};
+
+export default PublicRoute;
